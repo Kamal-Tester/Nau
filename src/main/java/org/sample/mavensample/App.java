d28a8d8e4b3796a0d0497c77310321caa459be44
unknown <meghani58155@gmail.com>
2025-05-27 13:28:16 +0530

    Updated

--- src/main/java/org/sample/mavensample/App.java
+++ src/main/java/org/sample/mavensample/App.java
@@ -1,12 +1,17 @@
 package org.sample.mavensample;
 
+import java.time.Duration;
 import java.util.Iterator;
 import java.util.Set;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Hello world
@@ -17,15 +22,23 @@ public class App
     public static void main( String[] args ) throws InterruptedException
     {
     	
-    	ChromeOptions options = new ChromeOptions();
-    	options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
-    	WebDriver driver = new ChromeDriver(options);
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dell\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+    	//ChromeOptions options = new ChromeOptions();
+    	//options.addArguments("--headless=new"); 
+    	//options.addArguments("--window-size=1920,1080");
+    	WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\ChromeDriver\\chromedriver.exe");
         
         driver.get("https://www.naukri.com/");
         driver.manage().window().maximize();
-        Thread.sleep(15000);
-        driver.findElement(By.xpath("//div//a[@id='login_Layer']")).click();
+ 
+        
+        
+        
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+        WebElement loginButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div/a[@class='nI-gNb-lg-rg__login']")));
+        loginButton.click();
+        
+        //driver.findElement(By.xpath("//div//a[@id='login_Layer']")).click();
         Thread.sleep(2000);
         driver.findElement(By.xpath("(//div/input[@type='text'])[1]")).sendKeys("meghani58155@gmail.com");
         driver.findElement(By.xpath("//input[@placeholder='Enter your password']")).sendKeys("Kamal@123");
@@ -33,9 +46,11 @@ public class App
         Thread.sleep(5000);
         String MainWindow=driver.getWindowHandle();	
         driver.findElement(By.xpath("//div[@class='view-profile-wrapper']/a[@href='/mnjuser/profile']")).click();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
         
-        driver.findElement(By.xpath("(//div/span[@class='edit icon'])[1]")).click();
+        WebDriverWait waitt = new WebDriverWait(driver, Duration.ofSeconds(5));
+        waitt.until(ExpectedConditions.elementToBeClickable(By.xpath("(//div/span[@class='edit icon'])[1]"))).click();
+        //driver.findElement(By.xpath("(//div/span[@class='edit icon'])[1]")).click();
         Thread.sleep(2000);
         	
         
@@ -43,7 +58,8 @@ public class App
        
         Thread.sleep(5000);
         driver.findElement(By.xpath("//div/textarea[@class='resumeHeadlineTxt materialize-textarea']")).clear();
-        driver.findElement(By.xpath("//div/textarea[@class='resumeHeadlineTxt materialize-textarea']")).sendKeys("Highly skilled Quality Assurance Engineer with 2+ years of experience in manual and automation testing. Proficient in Selenium, Cucumber, TestNG, SQL, and CI/CD with expertise in hybrid frameworks. Experienced in using Bitbucket for version control.");
+        driver.findElement(By.xpath("//div/textarea[@class='resumeHeadlineTxt materialize-textarea']")).sendKeys("Highly skilled Quality Assurance Engineer with 3+ years of experience in manual and automation testing. Proficient in Selenium, Cucumber, TestNG, SQL, and CI/CD with expertise in hybrid frameworks. Experienced in using Bitbucket for version control.");
+        System.out.println("Updated");
         Thread.sleep(2000);
         driver.findElement(By.xpath("(//div/button[@class='btn-dark-ot'])[3]")).click();
         Thread.sleep(5000);
